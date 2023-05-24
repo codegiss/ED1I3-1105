b237199a55d368c4e1c499a51650ef35cd72de2e
Giselle Souza <gisellesouza243@gmail.com>
2023-05-23 21:10:59 -0300

    Pilha ímpar - ponteiro

--- pilha.cpp
+++ pilha.cpp
@@ -9,25 +9,25 @@ struct Par {
     float elementos[MAX];
 };
 
-Par* init() {
+Par* initPar() {
     Par *p = new Par();
     p->num=0;
     return p;
 }
 
-int isEmpty(Par *p) {
+int isEmptyPar(Par *p) {
     return (p->num==0);
 }
 
-int count(Par *p) {
+int countPar(Par *p) {
     return (p->num);
 }
 
-void freePilha(Par *p) {
+void freePilhaPar(Par *p) {
     free(p);
 }
 
-int push(Par *p, float v) {
+int pushPar(Par *p, float v) {
     int podeEmpilhar = (p->num < MAX);
     if(podeEmpilhar) {
         p->elementos[p->num++] = v;
@@ -35,7 +35,7 @@ int push(Par *p, float v) {
     return podeEmpilhar;
 }
 
-float pop(Par *p) {
+float popPar(Par *p) {
     float v;
     int podeDesempilhar = !(isEmpty(p));
     if(podeDesempilhar) {
@@ -49,12 +49,88 @@ float pop(Par *p) {
 
 // pilha impar ponteiro
 
+struct Impar {
+    float dado;
+    struct Impar *prox;
+};
+
+struct Pilha {
+    Impar *topo;
+};
+
+Pilha* initImpar {
+    Pilha *p = new Pilha();
+	  p->topo = NULL;
+	  return p;
+}
+
+int isEmptyImpar(Pilha *p) {
+	  return (p->topo == NULL);
+}
+
+int countImpar(Pilha *p) {
+    int i = 0;
+	  No *no = p->topo;
+	  while (no != NULL) {
+		    i++;
+		    no = no->prox;
+	  }
+	  return i;
+}
+
+void printImpar(Pilha *p) {
+	  No *no = p->topo;
+	  cout << "--------------" << endl;
+	  cout << "Qtde de elementos: " << count(p) << endl;
+	  cout << "--------------" << endl;
+	  while (no != NULL) {
+		    cout << no->dado << endl;
+		    no = no->prox;
+	  }
+    cout << "--------------" << endl;
+}
+
+void pushImpar(Pilha *p, float v) {
+	  No *no = new No();
+	  no->dado = v;
+	  no->prox = p->topo;
+	  p->topo = no;
+}
+
+float popImpar(Pilha *p) {
+	  float ret;
+	  int podeDesempilhar = (!isEmpty(p));
+	  if (podeDesempilhar) {
+	      No *no = p->topo;
+		    ret = no->dado;
+		    p->topo = p->topo->prox;
+		free(no);
+	  }
+	  else {
+		    ret = -1;
+	  }
+	  return ret;
+}
+
+void freePilhaImpar(Pilha *p) {
+	  No *no = p->topo;
+	  while (no != NULL) {
+		    No *aux = no->prox;
+		    free(no);
+		    no = aux;
+	}
+	  free(p);
+}
+
 int main () {
     int cont=0, numero, anterior;
 
     Par *par;
     par = new Par();
-    par = init();
+    par = initPar();
+
+    Pilha *impar;
+    impar = initImpar();
 
     for(cont=1; cont<=30; cont++) {
         if(cont == 1) {
@@ -70,14 +146,18 @@ int main () {
         }
         
         if(numero%2 == 0) {
-            push(par, numero);
+            pushPar(par, numero);
         } else {
             // gravar na pilha impar
+            pushImpar(impar, numero);
         }
     }
 
     // mostrar os valores em sequencia decrescente
     // independente da pilha
+
+    freePilhaPar(par);
+    freePilhaImpar(impar);
 
     return 0;
 }
